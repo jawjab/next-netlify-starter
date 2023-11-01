78fc5e9bd5762b6cb5978f08f532e62d04da35b9
settapong.p <settapong.p@linecorp.com>
2023-11-01 10:51:08 +0700

    add send request

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import axios from 'axios';
 
+async function request() {
+  const response = await axios.get('https://4e52-2405-9800-b660-6706-a0bf-3946-1fef-d3c0.ngrok-free.app/hello')
+  console.log(response.data);
+}
 export default function Home() {
+  request()
   return (
     <div className="container">
       <Head>
